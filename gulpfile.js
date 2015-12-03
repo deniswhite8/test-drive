9b1c40fb89133c7d9531ea1e0c1c3e4b93d651c0
denis <deniswhite8@gmail.com>
2015-12-03 23:11:28 +0300

    Setting admin frontend build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
+    //build client app
     mix
         .less(['app.less'], null, {
             paths: __dirname + '/node_modules/'
@@ -8,5 +9,13 @@ elixir(function(mix) {
         .browserify('app.js')
         .version(['css/app.css', 'js/bundle.js'])
         .copy('node_modules/bootstrap/dist/fonts/**', elixir.config.publicDir + '/build/fonts')
+    ;
+
+    // build admin sleeping owl
+    mix
+        .less(['admin.less'], elixir.config.cssOutput + '/admin.css', {
+            paths: __dirname + '/node_modules/'
+        })
+        .version(['css/admin.css'])
     ;
 });
